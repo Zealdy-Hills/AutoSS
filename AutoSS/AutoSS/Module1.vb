8ba5dfddd1beb4d0f479d73d32807d547b51c884
Rizaldi Ahya <bsi90179@bsi.co.id>
2023-06-13 08:30:25 +0700

    Init

---
+++ AutoSS/AutoSS/Module1.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Windows.Media.Imaging
+
+Module Module1
+    Private Prefix As String = ""
+    Private Start As Boolean = False
+
+    Sub Main()
+        Console.WriteLine("Option...")
+        Console.WriteLine("1. Setting Prefix")
+        Console.WriteLine("2. Start")
+Start:
+        Select Case Console.ReadLine()
+            Case "1"
+                Prefix = Console.ReadLine()
+                GoTo Start
+            Case Else
+                Start = True
+                Console.Clear()
+                CoreSS()
+        End Select
+        Dim debug = ""
+    End Sub
+
+    Private Sub CoreSS()
+        While Start
+            If DateTime.Now.Second = 0 OrElse DateTime.Now.Second = 30 Then
+                Dim fileName As String = Prefix & " Jam" & DateTime.Now.Hour & "." & DateTime.Now.Minute & ".png"
+                Dim ss As BitmapImage = ConvertBitmapImage(TakeScreenShot())
+                SavePngImage(ss, fileName)
+                Console.WriteLine(fileName)
+                Threading.Thread.Sleep(1000)
+            End If
+        End While
+    End Sub
+
+    Private Function TakeScreenShot() As Bitmap
+        Dim screenSize As Size = New Size(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim screenGrab As New Bitmap(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim g As Graphics = Graphics.FromImage(screenGrab)
+        g.CopyFromScreen(New Point(0, 0), New Point(0, 0), screenSize)
+        Return screenGrab
+    End Function
+
+    Private Function ConvertBitmapImage(ByVal bitmap As Bitmap) As BitmapImage
+        Using memory = New MemoryStream()
+            bitmap.Save(memory, ImageFormat.Png)
+            memory.Position = 0
+
+            Dim bitmapImage = New BitmapImage()
+            bitmapImage.BeginInit()
+            bitmapImage.StreamSource = memory
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad
+            bitmapImage.EndInit()
+            bitmapImage.Freeze()
+
+            Return bitmapImage
+        End Using
+    End Function
+
+    Private Sub SavePngImage(source As BitmapImage, photoLocation As String)
+        SaveImage(source, photoLocation, New PngBitmapEncoder())
+    End Sub
+
+    Private Sub SaveImage(source As BitmapImage, photoLocation As String, encoder As BitmapEncoder)
+        encoder.Frames.Add(BitmapFrame.Create(source))
+        Using filestream = New FileStream(photoLocation, FileMode.Create)
+            encoder.Save(filestream)
+        End Using
+    End Sub
+
+End Module
---
+++ AutoSS/AutoSS/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AutoSS")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("AutoSS")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0b0b1267-a91a-410a-b501-ac33b545e1e8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
