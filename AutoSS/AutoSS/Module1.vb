e038bcdc01fb9d4aafd9e0459b3762be1402dcff
Rizaldi Ahya <bsi90179@bsi.co.id>
2024-08-20 07:31:15 +0700

    bug fix

--- AutoSS/AutoSS/Module1.vb
+++ AutoSS/AutoSS/Module1.vb
@@ -51,19 +51,17 @@ Start:
     End Function
 
     Private Function ConvertBitmapImage(ByVal bitmap As Bitmap) As BitmapImage
-        Using memory = New MemoryStream()
+        Dim bitmapImage = New BitmapImage()
+        Using memory As MemoryStream = New MemoryStream()
             bitmap.Save(memory, ImageFormat.Png)
             memory.Position = 0
-
-            Dim bitmapImage = New BitmapImage()
             bitmapImage.BeginInit()
             bitmapImage.StreamSource = memory
             bitmapImage.CacheOption = BitmapCacheOption.OnLoad
             bitmapImage.EndInit()
             bitmapImage.Freeze()
-
-            Return bitmapImage
         End Using
+        Return bitmapImage
     End Function
 
     Private Sub SavePngImage(source As BitmapImage, photoLocation As String)
